19b52a77ace389f0fc587f34716e9d5d14c52796
Ahmedfarghal <a.fegoo2@gmail.com>
2019-04-01 03:51:12 +0200

    Reactjs Search Filter

--- src/App.js
+++ src/App.js
@@ -1,28 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+export default class App extends Component {
+   state={
+      input: '',
+      countries: ['Algeria', 'Argentina', 'Australia', 'Austria', 'Belarus',
+        'Belgium', 'Brazil', 'Canada', 'Chile', 'Colombia', 'Denmark', 'Dominican', 'RepublicEcuador,', 'Egypt', 'France',
+        'Germany', 'Ghana', 'Greece', 'Guatemala', 'India', 'Indonesia', 'Ireland', 'Israel', 'Italy', 'Japan', 'Jordan', 'Kenya',
+        'Korea', 'Kuwait', 'Latvia', 'Lebanon', 'Malaysia', 'Mexico', 'Netherlands', 'New Zealand', 'Nigeria', 'Norway', 'Oman',
+        'Pakistan', 'Panama', 'Peru', 'Philippines', 'Poland', 'Portugal', 'Puerto', 'Rico', 'Qatar', 'Russia', 'Saudi Arabia', 'Singapore', 'South Africa', 'Spain', 'Sweden', 'Switzerland', 'Thailand', 'Turkey', 'Ukraine', 'United Arab Emirates',
+        'United Kingdom', 'United States', 'Venezuela', 'Vietnam'
+      ]
+    }
+
+   //search Handler 
+searchHandler(event){
+  this.setState({
+    input:event.target.value
+  }, console.log(event.target.value))
+}
+
   render() {
+    //filtering the countries 
+    let filterCountry = this.state.countries.filter(country => {
+      if(country.toLocaleLowerCase().indexOf(this.state.input.toLocaleLowerCase())!== -1){
+          return true;
+      }
+    })
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <form> 
+          <input 
+            type="text" 
+            onChange={this.searchHandler.bind(this)}
+            value={this.state.input}>
+          </input>
+        </form>
+        <ul className="grid-container">
+        {filterCountry.map(country => {
+          return <li className=" grid-item" key={country}>{country}</li>})}
+        </ul>
       </div>
     );
   }
 }
 
-export default App;
